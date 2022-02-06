8c21a5d5392b6fbb8a75663a7e41ff165e671668
jordan-creyelman <creyelmanjo@gmail.com>
2022-02-06 21:13:26 +0100

    start

--- lua/love/main.lua
+++ lua/love/main.lua
@@ -1,11 +1,17 @@
-position_y = 100 
-position_x=100
+pad_y = 0
+pad_x=0
 function love.load()
   
 end
 function love.update()
-  position_y = position_y +1
+  if love.keyboard.isDown("down") then
+    pad_y = pad_y +1
+  end
+  if love.keyboard.isDown("up") then
+    pad_y =pad_y -1
+  end  
+
 end
 function love.draw()
-  love.graphics.rectangle("fill",position_x,position_y,20,80)
+  love.graphics.rectangle("fill",pad_x,pad_y,20,80)
 end
